40496ab9155caf5e412895556d4373c8b289a5e7
bootchk <bootch@nc.rr.com>
2016-10-08 14:25:18 -0400

    Move comments to correct class.

--- src/syncAgent/modules/cliqueMerger.h
+++ src/syncAgent/modules/cliqueMerger.h
@@ -13,29 +13,7 @@
  * 1. know time of merge slot
  * 2. know the 'design' (state) of merger (attributes of the mergee clique.)
  *
- * CliqueMerger knows when to schedule a mergerSlot (with collision avoidance).
- * Note that many fishers may discover the same otherClique, try to merge it, and thus contend.
- * A unit sends MergeSync probablistically, to avoid contention.
- * The contention is with:
- * - master of otherClique (still sending Sync in its syncSlot)
- * - other mergers of otherClique
  *
- * Current design/implementation:
- *
- * A unit only sends one MergeSync.
- * If master of otherClique fails to hear, they will keep mastering the other clique,
- * and slaves of otherClique that failt to hear will remain in the other clique.
- * If master of otherClique hears but slaves of otherClique fail to hear,
- * slaves will fallout of sync, and resume mastership of otherClique
- * ( possibly remain in their own single member clique.)
- * In both cases the clique(s) must be fished again.
- *
- * Note however, that multiple units may have fished the same otherClique
- * and may be contending to merge it.
- *
- * Alternative implementation:
- *
- * Each unit repeatedly send MergeSync to increase chance other clique is merged quickly.
  */
 class CliqueMerger {
 
--- src/syncAgent/modules/mergePolicy.h
+++ src/syncAgent/modules/mergePolicy.h
@@ -9,6 +9,32 @@
  * Responsibilities
  * 1. know circumstances for scheduling MergeSlot (and xmitting MergeSync)
  * 2. know when to quit role Merger (how long to continue Merging another clique.)
+ *
+ * MergePolicy knows to send MergeSync with collision avoidance.
+ * A unit sends MergeSync probablistically to avoid contention.
+ *
+ * Many fishers may fish the same otherClique, assume role Merger, and contend to merge it.
+ * The contention is with:
+ * - master of otherClique (still sending Sync in its syncSlot)
+ * - other mergers of otherClique
+ *
+ * Choices:
+ * - a unit sends only one MergeSync, but with a slight random stagger from SyncPoint of otherClique.
+ * - a unit repeatedly sends many MergeSyncs in a sequence of SyncPeriods.
+ *
+ * The chance that many Mergers will be contending depends on:
+ * - the duty cycle
+ * - count of units
+ * - how early in the synch game it is
+ *
+ * TODO discuss each of these factors
+ *
+ * If master of otherClique fails to hear, they will keep mastering the other clique,
+ * and slaves of otherClique that fail to hear will remain in the other clique.
+ * If master of otherClique hears but slaves of otherClique fail to hear,
+ * slaves will fallout of sync, and resume mastership of otherClique
+ * ( possibly remain in their own single member clique.)
+ * In both cases the clique(s) must be fished again.
  */
  
  
