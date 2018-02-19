db7382365e619c541e5482abb7dadaa3a395ef81
bootchk <bootch@nc.rr.com>
2018-02-19 12:22:11 -0500

    API randUnsignedInt16 -> randUnsignedInt

--- src/policy/provisionManager.cpp
+++ src/policy/provisionManager.cpp
@@ -10,6 +10,14 @@ namespace {
 
 /*
  * Provision periodically, regularly.
+ *
+ * Assert this is not in step with other units (in real time),
+ * since each unit starts at a random real time.
+ *
+ * Also, any provisioning is randomized within a syncPeriod.
+ * I.E. this won't collide with any other unit provisioning, with high probablity.
+ *
+ * IOW, no need to randomize this also, using RandomAlarmingClock.
  */
 bool ProvisionManager::shouldProvision() {
 	bool result = false;
